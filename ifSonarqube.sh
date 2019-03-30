fe7c3b26fed25e5f159deb3efef6a78c58f4211d
马飞 <mf171760@alibaba-inc.com>
2019-03-30 12:09:03 +0800

    init

---
+++ ifSonarqube.sh
@@ -0,0 +1,27 @@
+
+#此脚本---用来一键启动Sonarqube环境
+
+#----------------------------------------------------------
+
+#判断是否存在Sonarqube环境是否启动
+docker ps | grep sonarqube  postgres &> /dev/null
+#如果没有启动，使用docker-compose启动相关的sonarqube容器
+if [ $? -ne 0 ]
+then
+	echo "sonarqube is not up,we will start up it!!!"
+   wget https://github.com/logan62334/sonarqube/raw/master/postgres_sonarqube.yml
+
+  # docker-compose -f postgres_sonarqube.yml up -d
+  docker-compose -f postgres_sonarqube.yml up 
+    
+
+else
+	echo "Sonarqube is up!!!"
+fi
+
+echo "---------------------------------------------------------"
+
+
+echo "请等待10分钟，sonarqube环境有点费时间！！！"
+
+echo "----------------------------------------------------------"
